e96e27e09b119f484414862da152f8759e387c23
Michael Deer <mick.deer@gmail.com>
2017-09-14 00:28:34 +0930

    Add post for todos

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet mum for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to market',
-	completed: false
-}, {
-	id: 3,
-	description: 'Have lunch',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
@@ -38,6 +31,18 @@ app.get('/todos/:id', function (req, res) {
 	} else {
 		res.status(404).send();
 	}
+});
+
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	console.log('description: ' + body.description);
+
+	body.id = todoNextId++;
+
+	todos.push(body);
+
+	res.json(body);
 });
 
 app.listen(PORT, function () {
