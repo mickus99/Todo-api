72ea0e5394e5eeafeb75f8d1de1315f279dd16c4
Michael Deer <mick.deer@gmail.com>
2017-09-14 13:48:50 +0930

    Add todo model and POST /todo

---
+++ playground/variable-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+// 	name: 'Mick',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Mick',
+// 	// 	age: 24
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+var grades = [15, 88];
+
+function addGrades (gradesArr) {
+	gradesArr.push(55);
+	debugger;
+
+	//gradesArr = [12, 33, 99];
+}
+
+addGrades(grades);
+console.log(grades)
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -52,17 +53,23 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
+	db.todo.create(body).then(function (todo) {
+		res.json(todo.toJSON());
+	}, function (e) {
+		res.status(400).json(e);
+	});
 
-	body.description = body.description.trim();
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
 
-	body.id = todoNextId++;
+	// body.description = body.description.trim();
 
-	todos.push(body);
+	// body.id = todoNextId++;
 
-	res.json(body);
+	// todos.push(body);
+
+	// res.json(body);
 });
 
 app.delete('/todos/:id', function(req, res) {
@@ -111,6 +118,8 @@ app.put('/todos/:id', function(req, res) {
 
 })
 
-app.listen(PORT, function() {
-	console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('Express listening on port ' + PORT + '!');
+	});
 });
